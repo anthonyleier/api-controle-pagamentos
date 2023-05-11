7b749628a6128f49b2133e86cb3ec3d244bf26e2
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-05-11 10:47:42 -0300

    Cadastro e listagem de usuários

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class UserController extends Controller {
+    public function index() {
+        $users = User::paginate();
+        return UserResource::collection($users);
+    }
+
+    public function store(StoreUpdateUserRequest $request) {
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        return new UserResource($user);
+    }
+}
