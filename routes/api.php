7a547a77a4ae8b278303adea1709736937d25a0b
Anthony Cruz <Anthonyleierlw@gmail.com>
2023-05-11 15:32:18 -0300

    Refatoração das rotas

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,4 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-Route::patch('/users/{id}', [UserController::class, 'update']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-
-Route::get('/', function () {
-    return response()->json([
-        'success' => true
-    ]);
-});
+Route::apiResource('/users', UserController::class);
